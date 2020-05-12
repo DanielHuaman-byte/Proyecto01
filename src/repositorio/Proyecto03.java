fb98032028ab7a8289106acaf737c9bf8ccdca65
DanielHuaman-byte <65198448+DanielHuaman-byte@users.noreply.github.com>
2020-05-12 01:59:38 -0500

    segundo avance del codigo
    segundo avance del codigo

--- src/repositorio/Proyecto03.java
+++ src/repositorio/Proyecto03.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto03;
+package repositorio;
 
 /**
  *
@@ -17,6 +17,15 @@ public class Proyecto03 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo 2.0xdxdxd");
+        
+        int n1 = 12 , n2 = 90 , mayor = 0;
+        if(n1>n2)
+            System.out.println("el mayor es :" + n1);
+        else
+            System.out.println("el mayor es : " + n2);
+        
+        
+        
     }
     
 }
